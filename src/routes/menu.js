b10d2359ce1d59838d85e59db3e07d9a01f18185
arcodez <abelileitor123@gmail.com>
2021-04-19 16:32:32 -0430

    update navbar

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,10 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 
-
-/* Obtener Todos los Usuarios */
 router.get("/menu", (req, res) => {
     res.render('menu/inicio', {layout: 'menu'})
+});
+router.get("/menu/login", (req, res) => {
+    res.render('menu/login', {layout: 'menu'})
+});
+router.get("/menu/products", (req, res) => {
+    res.render('menu/products', {layout: 'menu'})
+});
+router.get("/menu/accounts", (req, res) => {
+    res.render('menu/accounts', {layout: 'menu'})
+});
+router.get("/menu/add-product", (req, res) => {
+    res.render('menu/add-product', {layout: 'menu'})
+});
+router.get("/menu/edit-product", (req, res) => {
+    res.render('menu/edit-product', {layout: 'menu'})
 });
 
 module.exports = router;
